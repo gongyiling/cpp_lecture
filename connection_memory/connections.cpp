06d7eb8d757d3e52001be3fbd88cb51347e0337e
fractal <245044518@qq.com>
2021-09-19 15:16:33 +0800

    finalize connections.

--- connection_memory/connections.cpp
+++ connection_memory/connections.cpp
@@ -25,60 +25,77 @@ awaitable<void> wait_until(boost::asio::io_context& io_context, pred_t pred)
 	}
 }
 
-awaitable<void> connect(boost::asio::io_context& io_context, tcp::endpoint ep, int& step)
+awaitable<void> connect(boost::asio::io_context& io_context, tcp::endpoint ep, int& step, const std::string& fn)
 {
 	tcp::socket s(io_context);
-	boost::asio::socket_base::receive_buffer_size option(256 * 1024);
-	s.set_option(option);
+	s.open(tcp::v4());
 
 	co_await wait_until(io_context, [&](){return step == 1;});
-	std::cout << "connecting" << std::endl;
-	co_await s.async_connect(ep, boost::asio::use_awaitable);
+	std::cerr << "set receiving buffer" << std::endl;
+
+	boost::asio::socket_base::receive_buffer_size option(8 * 1024);
+	s.set_option(option);
 
 	co_await wait_until(io_context, [&](){return step == 2;});
-	std::cout << "writing" << std::endl;
-	std::string req = "GET / HTTP/1.1\r\n\r\n";
-	const size_t written_bytes = co_await boost::asio::async_write(s, boost::asio::const_buffer(req.data(), req.length()), boost::asio::use_awaitable);
+	std::cerr << "connecting" << std::endl;
+	co_await s.async_connect(ep, boost::asio::use_awaitable);
 
 	co_await wait_until(io_context, [&](){return step == 3;});
-	std::cout << "reading" << std::endl;
-	static char buf[10240];
-	const size_t read_bytes = co_await boost::asio::async_read(s, boost::asio::mutable_buffer(buf, sizeof(buf)), boost::asio::use_awaitable);
-	buf[read_bytes] = '\0';
-	std::cout << buf << std::endl;
+	std::cerr << "writing" << std::endl;
+	std::string req = "GET /" + fn + " HTTP/1.1\r\n\r\n";
+	const size_t written_bytes = co_await boost::asio::async_write(s, 
+		boost::asio::const_buffer(req.data(), req.length()), 
+		boost::asio::use_awaitable);
+
+	co_await wait_until(io_context, [&](){return step == 4;});
+	boost::asio::streambuf buf;
+	const size_t read_bytes = co_await boost::asio::async_read_until(s, 
+		buf, 
+		"\r\n\r\n",
+		boost::asio::use_awaitable);
+	std::cerr << "read_bytes: " << read_bytes << std::endl;
+
+	co_await wait_until(io_context, [&](){return step == 5;});
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-	boost::asio::io_context io_context;
-	
-	boost::asio::signal_set signals(io_context, SIGINT, SIGTERM);
+	try
+	{
+		boost::asio::io_context io_context;
+		
+		boost::asio::signal_set signals(io_context, SIGINT, SIGTERM);
 
-	int step = 0;
-
-	std::function<void (const boost::system::error_code&, int)> signal_handle = [&](const boost::system::error_code&, int)
-	{ 
-		if (step < 3) 
-		{
-			step++;
-			signals.async_wait(signal_handle);
-		}
-		else
+		int step = 0;
+		std::function<void (const boost::system::error_code&, int)> signal_handle = [&](const boost::system::error_code&, int)
 		{ 
-			io_context.stop(); 
+			if (step < 5) 
+			{
+				step++;
+				signals.async_wait(signal_handle);
+			}
+			else
+			{ 
+				io_context.stop(); 
+			}
+		};
+
+		signals.async_wait(signal_handle);
+
+		tcp::resolver resolver(io_context);
+    	tcp::resolver::results_type endpoints = resolver.resolve("192.168.3.38", "8000");
+		const int N = 10;	
+		for (int i = 0; i < N; i++)
+		{ 	
+			co_spawn(io_context, connect(io_context, *endpoints.begin(), step, argv[1]), detached);
 		}
-	};
-
-	signals.async_wait(signal_handle);
-
-	tcp::resolver resolver(io_context);
-    tcp::resolver::results_type endpoints = resolver.resolve("www.baidu.com", "80");
-	const int N = 1;	
-	for (int i = 0; i < N; i++)
-	{ 	
-		co_spawn(io_context, connect(io_context, *endpoints.begin(), step), detached);
+		
+		io_context.run();
 	}
-	
-	io_context.run();
+	catch(std::exception& e)
+  	{
+    	std::cerr << "Exception: " << e.what() << "\n";
+  	}
+	return 0;
 }
 
